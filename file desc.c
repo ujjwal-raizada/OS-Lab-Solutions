77b3b667188f287c34de1fdb96ff71b06332280e
Ujjwal Raizada <ujjwalraizada@gmail.com>
2019-11-16 15:06:00 +0530

    file desc

---
+++ file desc.c
@@ -0,0 +1,40 @@
+// 0 -> stdin
+// 1 -> stdout
+// 2 -> stderr
+
+#include<stdio.h>
+#include<unistd.h>
+#include<sys/types.h>
+#include<sys/wait.h>
+#include <errno.h>
+
+int main() {
+	pid_t pid;
+	int pfd[2];
+	int s;
+	pipe(pfd);
+	pid = fork();
+
+	if (pid == 0) {
+		dup2(pfd[1], 1);
+		close(pfd[0]);
+		close(pfd[1]);
+		if (execlp("ls", "ls", NULL) == -1)
+			perror("execlp ls");
+	}
+	else {
+		if (fork() == 0) {
+			dup2(pfd[0], 0);
+			close(pfd[0]);
+			close(pfd[1]);
+			if (execlp("wc", "wc", "-l", NULL) == -1)
+				perror("execlp wc");
+		}
+		else {
+			close(pfd[0]);
+			close(pfd[1]);
+			wait(&s);
+			wait(&s);
+		}
+	}
+}
