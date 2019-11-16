d940d2ac610442dc5a2da33063b9e778603e7a51
Ujjwal Raizada <ujjwalraizada@gmail.com>
2019-11-16 23:04:08 +0530

    add process for reader writer with a bug

--- message queue.c
+++ message queue.c
@@ -15,7 +15,6 @@ struct msgbuf {
 };
 
 int main(void) {
-	struct msgbuf buf;
 	int msgid;
 	key_t key;
 
@@ -24,21 +23,59 @@ int main(void) {
 		exit(1);
 	}
 
-	if ((msgid = msgget(key, 0644 | IPC_CREAT)) == -1) {
-		perror("msgget");
-		exit(1);
-	}
-
-	printf("Reader: ready to receive messages, boss.\n");
-
-	while (1) {
-
-		if (msgrcv(msgid, &buf, sizeof(buf.mtext), 0, 0) == -1) {
-			perror("msgrcv");
+	if (fork() == 0) {
+		struct msgbuf buf;
+		// writer
+		if ((msgid = msgget(key, 0644 | IPC_CREAT)) == -1) {
+			perror("msgget");
 			exit(1);
 		}
-		printf("reader: \"%s\"\n", buf.mtext);
+
+		printf("Enter lines of text: ^D to quit.\n");
+
+		buf.mtype = 1;
+
+		while (fgets(buf.mtext, sizeof(buf.mtext), stdin) != NULL) {
+			printf("1");
+			int len = strlen(buf.mtext);
+
+			if (buf.mtext[len - 1] == '\n')
+				buf.mtext[len - 1] == '\0';
+
+			if (msgsnd(msgid, &buf, len + 1, 0) == -1)
+				perror("msgsnd");
+			sleep(2);
+			printf("2");
+		}
+		if (msgctl(msgid, IPC_RMID, NULL) == -1) {
+			perror("msgctl");
+			exit(1);
+		}
+		printf("3");
 
 	}
+	else {
+		struct msgbuf buf;
+		// reader
+		if ((msgid = msgget(key, 0644 | IPC_CREAT)) == -1) {
+			perror("msgget");
+			exit(1);
+		}
+
+		printf("Reader: ready to receive messages, boss.\n");
+
+		while (1) {
+			printf("4");
+			if (msgrcv(msgid, &buf, sizeof(buf.mtext), 0, 0) == -1) {
+				perror("msgrcv");
+				exit(1);
+			}
+			printf("5");
+			printf("reader: \"%s\"\n", buf.mtext);
+
+		}
+	}
+
+	
 	return 0;
 }
