a52092017af786cdcddc22b9899eb0307a5e72e8
Ujjwal Raizada <ujjwalraizada@gmail.com>
2019-11-17 01:30:59 +0530

    sum numbers

--- threads.c
+++ threads.c
@@ -2,29 +2,31 @@
 #include<stdio.h>
 #include<stdlib.h>
 
-#define NUM_THREADS 5
+int sum = 0;
 
-void *printHello(void *threadid) {
-	long tid;
-	tid = (long)threadid;
-	printf("Hello World!, thread: %ld\n", tid);
+void *printHello(void *num) {
+	sum = sum + *((int*)num);
 	sleep(1);
 	pthread_exit(NULL);
 }
 
-int main() {
+int main(int argc, char* argv[]) {
+	int NUM_THREADS = argc - 1;
 	pthread_t threads[NUM_THREADS];
 	int rc;
 	long t;
 
 	for (t = 0; t < NUM_THREADS; t++) {
 		printf("In main: creating thread %ld\n", t);
-		rc = pthread_create(&threads[t], NULL, printHello, (void *)t);
+		int num = atoi(argv[t + 1]);
+		printf("%d\n", num);
+		rc = pthread_create(&threads[t], NULL, printHello, (void *)(&num));
 		if (rc) {
 			printf("ERROR: %d in thread_create.\n", rc);
 			exit(1);
 		}
 		pthread_join(threads[t], NULL);
+		printf("sum: %d\n", sum);
 		printf("exiting main thread.\n");
 	}
 	pthread_exit(NULL);
